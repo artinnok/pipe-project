ee77eaff576ae6fc825ca0deb0651cc005e634bb
Artem Innokentiev <artinnok@mail.ru>
2016-03-30 16:36:29 +0300

    refactoring

--- pipe.py
+++ pipe.py
@@ -32,12 +32,12 @@ class NPS:
         self.diff = self.out - self.inp
         self.pump = Data(pump).get_pump()
 
-    def get_predict(self):
+    def get_predict(self):  # линейная регрессия
         regr = LinearRegression()
         regr.fit(self.flow, self.diff)
         return regr.predict(self.flow)
 
-    def get_pump_count(self) -> dict:  # {1: 4, 2: 0, 3: 21, 4: 7}
+    def get_pump_count(self) -> dict:  # {1: 4, 2: 0, 3: 21, 4: 7}, {насос: сколько раз встречается}
         inp = []
         [inp.extend(item) for item in self.pump]
         out = {
@@ -48,14 +48,30 @@ class NPS:
         }
         return out
 
-    def get_unique_modes(self) -> list:  # [[1,3], [2,4]
+    def get_flow_diff_of_pump(self, pump: int):  # напор и разница давления одного насоса
+        flow, diff = [], []
+        for index, item in enumerate(self.pump):
+            if pump in item:
+                flow.append(self.flow[index])
+                diff.append(self.diff[index])
+        return [flow, diff]
+
+    def get_flow_diff_all_pumps(self):  # вернет словарь {насос: [массив из расхода, массив из давления]}
+        out = {
+            1: self.get_flow_diff_of_pump(1),
+            2: self.get_flow_diff_of_pump(2),
+            3: self.get_flow_diff_of_pump(3),
+            4: self.get_flow_diff_of_pump(4)
+        }
+        return out
+
+    def get_unique_modes(self) -> list:  # [[1,3], [2,4]] вернет уникальные режимы
         out = []
         [out.append(item) for item in self.pump if item not in out]
         return out
 
-    def get_flow_diff_of_mode(self, mode: list):
-        flow = []
-        diff = []
+    def get_flow_diff_of_mode(self, mode: list) -> list:   # [[f1, f2 ...], [dp1, dp2 ...]]
+        flow, diff = [], []              # вернет расход и давление одного режима [массив из расхода, массив из давлений]
         for index, item in enumerate(self.pump):
             if item == mode:
                 flow.append(self.flow[index])
@@ -71,20 +87,13 @@ class NPS:
             plt.title('Режим ' + str(item))
         plt.show()
 
-    def get_pump_flow(self):
-        out = {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': []
-        }
 
 us = Linear('Y', 'AG', 'K')
 
 ukhta = NPS('W', 'Y', 'K', 'U')
 sindor = NPS('AG', 'AI', 'K', 'AE')
 
-ukhta.show_modes()
+print(ukhta.get_flow_diff_all_pumps())
 
 # plt.scatter(ukhta.flow, ukhta.diff, s=50)  # исходные точки
 # plt.plot(ukhta.flow, ukhta.get_predict())  # линейная регрессия
