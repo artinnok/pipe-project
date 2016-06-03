0a4f7d9a3205c4a437142c6b04e536b374940f33
Artem Innokentiev <artinnok@mail.ru>
2016-06-03 16:57:36 +0300

    fixies + refactoring + new methods

--- pipe.py
+++ pipe.py
@@ -8,7 +8,7 @@ font = {'family': 'Verdana', 'weight': 'normal'}
 plt.rc('font', **font)
 
 # установка формата вывода
-np.set_printoptions(precision=6, suppress=True, linewidth=150)
+np.set_printoptions(precision=10, suppress=True, linewidth=150)
 
 P11 = 2 * 10 ** 5
 PN1 = 1 * 10 ** 5
@@ -18,9 +18,9 @@ X = np.array([[[P11], [PN1]], [[P12], [PN2]]])
 # X = np.array([[P11], [PN1]])
 B1 = 10 ** (-3)
 B0 = 0
-THETA = np.array([[B0], [B1]])
+THETA = np.array([[B0], [B0], [B1], [B1]])
 
-H = 10 ** (-6)
+h = 10 ** (-6)
 E = 0.05
 
 
@@ -33,24 +33,24 @@ class Solver:
         :return:
         """
         l = len(theta) / 2
-        b0 = theta[:l].reshape(l, 1)
-        b1 = theta[l:].reshape(l, 1)
+        beta0 = theta[:l].reshape(l, 1)
+        beta1 = theta[l:].reshape(l, 1)
         n = int(l + 1)
 
-        a2t = helper.get_a2(n)
-        a2t = helper.crutch(-b1, a2t)  # костыль
-        ones = -np.ones((len(b1), 1))  # костыль
-        top = np.append(ones, a2t, 1)
+        A2T = helper.get_a2(n)
+        A2T = helper.crutch(-beta1, A2T)  # костыль
+        ones = -np.ones((len(beta1), 1))  # костыль
+        top = np.append(ones, A2T, 1)
 
-        a1t = helper.get_a1(n)
+        A1T = helper.get_a1(n)
         zeros = np.zeros((2, 1))
-        bottom = np.append(zeros, a1t, 1)
+        bottom = np.append(zeros, A1T, 1)
 
-        d = np.append(top, bottom, 0)
-        dinv = np.linalg.inv(d)
-        u = np.append(-b0, x, 0)
-        w = np.dot(dinv, u)
-        return w
+        D = np.append(top, bottom, 0)
+        Dinv = np.linalg.inv(D)
+        u = np.append(-beta0, x, 0)
+        v = np.dot(Dinv, u)
+        return v
 
     def solve(self, theta, x):
         """
@@ -73,11 +73,11 @@ class Solver:
         :return:
         """
         copy_theta = np.array(theta, copy=True)
-        copy_theta[index] += H
-        f_mod = self.single_solve(copy_theta, x)
-        f = self.single_solve(theta, x)
-        e = f_mod - f
-        e /= H
+        copy_theta[index] += h
+        v_mod = self.single_solve(copy_theta, x)
+        v = self.single_solve(theta, x)
+        e = v_mod - v
+        e /= h
         return e
 
     def singe_jacobian(self, theta, x):
@@ -106,21 +106,53 @@ class Solver:
         )
         return out
 
-    def delta_theta(self, y, theta, x):
+    def get_ls_theta(self, y, theta, x):
         """
-        Вернет оценку дельта тета
+        МНК
         :param theta:
         :param y:
         :param x:
         :return:
         """
-        ws = self.solve(theta, x)
-        jacobian = self.jacobian(theta, x)
-        a = np.dot(jacobian.T, jacobian)
-        ainv = np.linalg.inv(a)
-        delta_theta = np.dot(ainv, jacobian.T)
-        e = y - ws
+        v = self.solve(theta, x)
+        H = self.jacobian(theta, x)
+        A = np.dot(H.T, H)
+        Ainv = np.linalg.inv(A)
+        delta_theta = np.dot(Ainv, H.T)
+        e = y - v
         delta_theta = np.dot(delta_theta, e)
+        if globals().get('errors') is not None:
+            globals()['errors'] = np.append(globals().get('errors'), e, axis=1)
+        else:
+            globals()['errors'] = e
+        return delta_theta
+
+    def weight(self, theta, x):
+        l = len(theta) / 2
+        l += 2
+        w = np.ones(l)
+        w[0] = 1000
+
+        n = len(x)
+        W = np.concatenate([w for item in range(n)])
+        W = np.diag(W)
+        return W
+
+    def get_wls(self, theta, x):
+        """
+        Взвешенный МНК
+        :param theta:
+        :param x:
+        :return:
+        """
+        H = self.jacobian(theta, x)
+        W = self.weight(theta, x)
+        Q = np.dot(W.T, W)
+        A = np.dot(H.T, Q)
+        A = np.dot(A, H)
+        Ainv = np.linalg.inv(A)
+        delta_theta = np.dot(Ainv, H.T)
+        delta_theta = np.dot(delta_theta, Q)
         return delta_theta
 
     def get_some_value(self, data):
@@ -128,23 +160,24 @@ class Solver:
         squares_sum = sum(list(squares))
         return sqrt(squares_sum) / len(data)
 
-
 s = Solver()
-print(s.solve(THETA, X))
+F = s.solve(THETA, X)
+H = s.jacobian(THETA, X)
 
-"""
-y, yy = s.get_ws_jacobians(B, X)
-print(y)
-y[0, 0] += 50
-delta_B = s.get_delta_b(y, B, X)
-delta = delta_B
-B = B + delta_B
+Y = np.array(F, copy=True)
+Y[0, 0] += 50
+Y[4, 0] += 50
+Y[2, 0] += 15000
+Y[6, 0] += 25000
+
+delta_theta = s.delta_theta(Y, THETA, X)
+delta = delta_theta
+theta = THETA + delta_theta
 some_value = s.get_some_value(delta)
 
-
 while some_value > E:
-    delta_B = s.get_delta_b(y, B, X)
-    delta = np.append(delta, delta_B, axis=0)
-    B = B + delta_B
+    delta_theta = s.delta_theta(Y, theta, X)
+    delta = np.append(delta, delta_theta, axis=0)
+    theta = theta + delta_theta
     some_value = s.get_some_value(delta)
-    """
+    s.weight(theta, X)
